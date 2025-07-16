3bbf3dcb28bc7f655a71aee442461fed137606c7
RohannaHasselkus <rohanna.hasselkus@gmail.com>
2025-07-16 13:51:29 -0400

    debugging test 3 Jul 16

--- tests/testthat/test-unagi.R
+++ tests/testthat/test-unagi.R
@@ -6,6 +6,12 @@ test_that("[UNAGI] SignificantBreadthFirstSearchU() function yields expected res
   # Here, we expect genes A and B to be connected after 1 hop via TF2, genes A and D
   # to be connected after 1 hop via TF3, and genes A and C to be connected after 2
   # hops via TF4.
+  bindWithReversed <- function(dataFrame){
+    reversedDataFrame <- dataFrame
+    reversedDataFrame$source <- dataFrame$target
+    reversedDataFrame$target <- dataFrame$source
+    return(rbind(dataFrame, reversedDataFrame))
+  }
   startingNetwork <- data.frame(source = c(rep(c("gene1", "gene2", "gene3", "gene4"), 4)),
                                 target = c(rep("geneA", 4), rep("geneB", 4), rep("geneC", 4), rep("geneD", 4)),
                            score = c(-3, 3, 5, -5, 0, 4, 0.0005, -0.5, 0, 0.0005, -1, 4, 0.0005, -2, 5, 3))
@@ -29,19 +35,16 @@ test_that("[UNAGI] SignificantBreadthFirstSearchU() function yields expected res
   
   # Create a set of networks close to the original.
   fullNetworks <- fullNetwork1
-  fullNetworks[,4] <- fullNetwork1$score - 0.00005
-  fullNetworks[,5] <- fullNetwork1$score + 0.00005
-  
+  fullNetworks$sample1 <- fullNetwork1$score - 0.00005
+  fullNetworks$sample2 <- fullNetwork1$score + 0.00005
+  fullNetworks <- bindWithReversed(fullNetworks)
+
   # Use all combined scores as the null distribution.
   null <- stats::rnorm(n = nrow(fullNetworks) * 3) / 100
   
   # Test that errors are thrown when appropriate.
   expect_error(SignificantBreadthFirstSearchU(networks = subnetwork,
                                              startingNodes = c("blob", "fish"),
-                                             nodesToExclude = c()),
-               "ERROR: Starting nodes do not overlap with network nodes")
-  expect_error(SignificantBreadthFirstSearchU(networks = subnetwork, 
-                                             startingNodes = c("gene1", "gene2"),
                                              nodesToExclude = c()),
                "ERROR: Starting nodes do not overlap with network nodes")
   expect_error(SignificantBreadthFirstSearchU(networks = subnetwork, 
@@ -54,45 +57,45 @@ test_that("[UNAGI] SignificantBreadthFirstSearchU() function yields expected res
                "ERROR: Starting nodes cannot overlap with nodes to exclude")
   
   # Ensure that, when starting from genes A, B, and C, we obtain the correct values.
-  expect_true(length(setdiff(c("gene2__geneA", "gene2__geneB", "gene3__geneA", "gene4__geneC"),
-                      rownames(SignificantBreadthFirstSearchU(networks = subnetwork,
-                                                  startingNodes = c("geneA", "geneB", "geneC"),
-                                                  nodesToExclude = c())))) == 0)
+  result1 <- SignificantBreadthFirstSearchU(networks = subnetwork,
+                                            startingNodes = c("geneA", "geneB", "geneC"),
+                                            nodesToExclude = c())
+  expect_setequal(rownames(result1), c("gene2__geneA", "gene2__geneB", "gene3__geneA", "gene4__geneC"))
   
   # Ensure that, when starting from genes gene2, gene3, and gene4 and removing genes A, B, and C, we obtain the correct values.
-  expect_true(length(setdiff(c("gene3__geneD", "gene4__geneD"),
-                             rownames(SignificantBreadthFirstSearchU(networks = subnetwork,
-                                                                    startingNodes = c("gene2", "gene3", "gene4"),
-                                                                    nodesToExclude = c("geneA", "geneB", "geneC"))))) == 0)
+  result2 <- SignificantBreadthFirstSearchU(networks = subnetwork,
+                                            startingNodes = c("gene2", "gene3", "gene4"),
+                                            nodesToExclude = c("geneA", "geneB", "geneC"))
+  expect_setequal(rownames(result2), c("gene3__geneD", "gene4__geneD"))
   
   # Unipartite structure.
-  startingNetwork <- data.frame(source = c(rep(c("gene1", "gene2", "gene3", "gene4"), 4)),
-                                target = c(rep("geneA", 4), rep("geneB", 4), rep("geneC", 4), rep("geneD", 4)),
-                                score = c(-3, 3, 5, -5, 0, 4, 0.0005, -0.5, 0, 0.0005, -1, 4, 0.0005, -2, 5, 3))
-  addedNoise1 <- data.frame(source = rep(1:100, 4), target = c(rep("geneA", 100), 
-                                                               rep("geneB", 100),
-                                                               rep("geneC", 100),
-                                                               rep("geneD", 100)),
-                            score = stats::rnorm(400) / 1000)
+  # startingNetwork <- data.frame(source = c(rep(c("gene1", "gene2", "gene3", "gene4"), 4)),
+  #                               target = c(rep("geneA", 4), rep("geneB", 4), rep("geneC", 4), rep("geneD", 4)),
+  #                               score = c(-3, 3, 5, -5, 0, 4, 0.0005, -0.5, 0, 0.0005, -1, 4, 0.0005, -2, 5, 3))
+  # addedNoise1 <- data.frame(source = rep(1:100, 4), target = c(rep("geneA", 100), 
+  #                                                              rep("geneB", 100),
+  #                                                              rep("geneC", 100),
+  #                                                              rep("geneD", 100)),
+  #                           score = stats::rnorm(400) / 1000)
   
-  geneANetHop1 <- data.frame(source = c("gene2", "gene3","geneB"), target = c("geneA", "geneA", "geneA"))
-  geneBNetHop1 <- data.frame(source = "gene2", target = "geneB")
-  geneCNetHop1 <- data.frame(source = c("gene4","geneD"), target = c("geneC", "geneC"))
-  geneDNetHop1 <- data.frame(source = c("gene3", "gene4"), target = c("geneD", "geneD"))
-  gene2NetHop1 <- data.frame(source = c("geneA", "geneB", "gene3"), target=c("gene2", "gene2", "gene2"))
-  geneANetHop2 <- rbind(geneBNetHop1, geneDNetHop1[1,])
-  geneBNetHop2 <- geneANetHop1[1,]
-  geneCNetHop2 <- geneDNetHop1[2,]
-  geneDNetHop2 <- rbind(geneANetHop1[2,], geneCNetHop1)
-  geneANetHop3 <- geneDNetHop1[2,]
-  geneBNetHop3 <- geneANetHop1[2,]
-  geneCNetHop3 <- geneDNetHop1[1,]
-  geneDNetHop3 <- geneANetHop1[1,]
-  
-  subnetworks <- list(geneA = list(geneANetHop1, geneANetHop2, geneANetHop3),
-                      geneB = list(geneBNetHop1, geneBNetHop2, geneBNetHop3),
-                      geneC = list(geneCNetHop1, geneCNetHop2, geneCNetHop3),
-                      geneD = list(geneDNetHop1, geneDNetHop2, geneDNetHop3))
+  # geneANetHop1 <- data.frame(source = c("gene2", "gene3","geneB"), target = c("geneA", "geneA", "geneA"))
+  # geneBNetHop1 <- data.frame(source = "gene2", target = "geneB")
+  # geneCNetHop1 <- data.frame(source = c("gene4","geneD"), target = c("geneC", "geneC"))
+  # geneDNetHop1 <- data.frame(source = c("gene3", "gene4"), target = c("geneD", "geneD"))
+  # gene2NetHop1 <- data.frame(source = c("geneA", "geneB", "gene3"), target=c("gene2", "gene2", "gene2"))
+  # geneANetHop2 <- rbind(geneBNetHop1, geneDNetHop1[1,])
+  # geneBNetHop2 <- geneANetHop1[1,]
+  # geneCNetHop2 <- geneDNetHop1[2,]
+  # geneDNetHop2 <- rbind(geneANetHop1[2,], geneCNetHop1)
+  # geneANetHop3 <- geneDNetHop1[2,]
+  # geneBNetHop3 <- geneANetHop1[2,]
+  # geneCNetHop3 <- geneDNetHop1[1,]
+  # geneDNetHop3 <- geneANetHop1[1,]
+  # 
+  # subnetworks <- list(geneA = list(geneANetHop1, geneANetHop2, geneANetHop3),
+  #                     geneB = list(geneBNetHop1, geneBNetHop2, geneBNetHop3),
+  #                     geneC = list(geneCNetHop1, geneCNetHop2, geneCNetHop3),
+  #                     geneD = list(geneDNetHop1, geneDNetHop2, geneDNetHop3))
 })
 test_that("[UNAGI] FindConnectionsForAllHopCountsU() function yields expected results", {
   
@@ -142,7 +145,7 @@ test_that("[UNAGI] FindConnectionsForAllHopCountsU() function yields expected re
   
   if (is.data.frame(result) || is.matrix(result)) {
     expect_equal(rownames(result), 
-                 c("gene3__geneD", "gene4__geneD", "geneA__gene3", "gene4__geneD"))
+                 c("gene3__geneD", "gene4__geneD", "geneA__gene3", "geneC__gene4"))
   } else {
     stop("Error: FindConnectionsForAllHopCountsU did not return a data frame or matrix.")
   }
@@ -153,10 +156,16 @@ test_that("[UNAGI] FindSignificantEdgesforhopU() function yields expected result
   # Here, we expect genes A and B to be connected after 1 hop via gene2, genes A and D
   # to be connected after 1 hop via TF3, and genes A and C to be connected after 2
   # hops via TF4.
+  bindWithReversed <- function(dataFrame){
+    reversedDataFrame <- data.frame(source = dataFrame$target, target = dataFrame$source)
+    return(rbind(dataFrame, reversedDataFrame))
+  }
+  subnetwork <- fullNetworks[, 1:3] 
+  
   startingNetwork <- data.frame(source = c(rep(c("gene1", "gene2", "gene3", "gene4"), 4)),
                                 target = c(rep("geneA", 4), rep("geneB", 4), rep("geneC", 4), rep("geneD", 4)),
                                 score = c(-3, 3, 5, -5, 0, 4, 0.0005, -0.5, 0, 0.0005, -1, 4, 0.0005, -2, 5, 3))
-  addedNoise1 <- data.frame(source = rep(1:100, 4), gene = c(rep("geneA", 100), 
+  addedNoise1 <- data.frame(source = rep(1:100, 4), target = c(rep("geneA", 100), 
                                                          rep("geneB", 100),
                                                          rep("geneC", 100),
                                                          rep("geneD", 100)),
@@ -172,47 +181,95 @@ test_that("[UNAGI] FindSignificantEdgesforhopU() function yields expected result
                                                          rep("geneD", 100)),
                             score = stats::rnorm(400) / 10000)
   fullNetwork1 <- rbind(startingNetwork, addedNoise1)
-  rownames(fullNetwork1) <- paste(fullNetwork1$gene, fullNetwork1$gene, sep = "__")
+  rownames(fullNetwork1) <- paste(fullNetwork1$source, fullNetwork1$target, sep = "__")
   
   # Create a set of networks close to the original.
   fullNetworks <- fullNetwork1
-  fullNetworks[,4] <- fullNetwork1$score - 0.00005
-  fullNetworks[,5] <- fullNetwork1$score + 0.00005
+  fullNetworks$sample1 <- fullNetwork1$score - 0.00005
+  fullNetworks$sample2 <- fullNetwork1$score + 0.00005
+  fullNetworks <- bindWithReversed(fullNetworks)
   
   #  Null distribution is a narrower version of the normal distribution.
   null <- rnorm(n = nrow(fullNetworks) * 3) / 100
   
+  # Construct a starting network, which will be modified.
+  # Here, we expect genes A and B to be connected after 1 hop via gene2, genes A and D
+  # to be connected after 1 hop via TF3, and genes A and C to be connected after 2
+  # hops via TF4.
+  bindWithReversed <- function(dataFrame){
+    reversedDataFrame <- data.frame(source = dataFrame$target, target = dataFrame$source)
+    return(rbind(dataFrame, reversedDataFrame))
+  }
+  startingNetwork <- data.frame(source = c(rep(c("gene1", "gene2", "gene3", "gene4"), 4)),
+                                target = c(rep("geneA", 4), rep("geneB", 4), rep("geneC", 4), rep("geneD", 4)),
+                                score = c(-3, 3, 5, -5, 0, 4, 0.0005, -0.5, 0, 0.0005, -1, 4, 0.0005, -2, 5, 3))
+  addedNoise1 <- data.frame(source = rep(1:100, 4), target = c(rep("geneA", 100), 
+                                                         rep("geneB", 100),
+                                                         rep("geneC", 100),
+                                                         rep("geneD", 100)),
+                            score = stats::rnorm(400) / 10000)
+  addedNoise2 <- data.frame(source = rep(1:100, 4), target = c(rep("geneA", 100), 
+                                                         rep("geneB", 100),
+                                                         rep("geneC", 100),
+                                                         rep("geneD", 100)),
+                            score = stats::rnorm(400) / 10000)
+  addedNoise3 <- data.frame(source = rep(1:100, 4), target = c(rep("geneA", 100), 
+                                                         rep("geneB", 100),
+                                                         rep("geneC", 100),
+                                                         rep("geneD", 100)),
+                            score = stats::rnorm(400) / 10000)
+  fullNetwork1 <- rbind(startingNetwork, addedNoise1)
+  rownames(fullNetwork1) <- paste(fullNetwork1$source, fullNetwork1$target, sep = "__")
+  
+  bindWithReversed <- function(dataFrame){
+    reversedDataFrame <- data.frame(source = dataFrame$target, target = dataFrame$source)
+    return(rbind(dataFrame, reversedDataFrame))
+  }
+  # Create a set of networks close to the original.
+  fullNetworks <- fullNetwork1
+  fullNetworks$sample1 <- fullNetwork1$score - 0.00005
+  fullNetworks$sample2 <- fullNetwork1$score + 0.00005
+  fullNetworks <- bindWithReversed(fullNetworks)
+  
+  #  Null distribution is a narrower version of the normal distribution.
+  null <- rnorm(n = nrow(fullNetworks) * 3) / 100
+  
+  bindWithReversed <- function(dataFrame){
+    reversedDataFrame <- data.frame(source = dataFrame$target, target = dataFrame$source)
+    return(rbind(dataFrame, reversedDataFrame))
+  }
+
   # Set up the subnetwork from the previous example.
   geneANetHop1 <- data.frame(source = c("gene2", "gene3"), target = c("geneA", "geneA"))
-  rownames(geneANetHop1) <- paste(geneANetHop1$gene, geneANetHop1$gene, sep = "__")
+  rownames(geneANetHop1) <- paste(geneANetHop1$source, geneANetHop1$target, sep = "__")
   geneBNetHop1 <- data.frame(source = "gene2", target = "geneB")
-  rownames(geneBNetHop1) <- paste(geneBNetHop1$gene, geneBNetHop1$gene, sep = "__")
+  rownames(geneBNetHop1) <- paste(geneBNetHop1$source, geneBNetHop1$target, sep = "__")
   geneCNetHop1 <- data.frame(source = "gene4", target = "geneC")
-  rownames(geneCNetHop1) <- paste(geneCNetHop1$gene, geneCNetHop1$gene, sep = "__")
+  rownames(geneCNetHop1) <- paste(geneCNetHop1$source, geneCNetHop1$target, sep = "__")
   geneDNetHop1 <- data.frame(source = c("gene3", "gene4"), target = c("geneD", "geneD"))
-  rownames(geneDNetHop1) <- paste(geneDNetHop1$gene, geneDNetHop1$gene, sep = "__")
+  rownames(geneDNetHop1) <- paste(geneDNetHop1$source, geneDNetHop1$target, sep = "__")
   geneANetHop2 <- rbind(geneBNetHop1, geneDNetHop1[1,])
-  rownames(geneANetHop2) <- paste(geneANetHop2$gene, geneANetHop2$gene, sep = "__")
+  rownames(geneANetHop2) <- paste(geneANetHop2$source, geneANetHop2$target, sep = "__")
   geneBNetHop2 <- geneANetHop1[1,]
-  rownames(geneBNetHop2) <- paste(geneBNetHop2$gene, geneBNetHop2$gene, sep = "__")
+  rownames(geneBNetHop2) <- paste(geneBNetHop2$source, geneBNetHop2$target, sep = "__")
   geneCNetHop2 <- geneDNetHop1[2,]
-  rownames(geneCNetHop2) <- paste(geneCNetHop2$gene, geneCNetHop2$gene, sep = "__")
+  rownames(geneCNetHop2) <- paste(geneCNetHop2$source, geneCNetHop2$target, sep = "__")
   geneDNetHop2 <- rbind(geneANetHop1[2,], geneCNetHop1)
-  rownames(geneDNetHop2) <- paste(geneDNetHop2$gene, geneDNetHop2$gene, sep = "__")
+  rownames(geneDNetHop2) <- paste(geneDNetHop2$source, geneDNetHop2$target, sep = "__")
   geneANetHop3 <- geneDNetHop1[2,]
-  rownames(geneANetHop3) <- paste(geneANetHop3$gene, geneANetHop3$gene, sep = "__")
+  rownames(geneANetHop3) <- paste(geneANetHop3$source, geneANetHop3$target, sep = "__")
   geneBNetHop3 <- geneANetHop1[2,]
-  rownames(geneBNetHop3) <- paste(geneBNetHop3$gene, geneBNetHop3$gene, sep = "__")
+  rownames(geneBNetHop3) <- paste(geneBNetHop3$source, geneBNetHop3$target, sep = "__")
   geneCNetHop3 <- geneDNetHop1[1,]
-  rownames(geneCNetHop3) <- paste(geneCNetHop3$gene, geneCNetHop3$gene, sep = "__")
+  rownames(geneCNetHop3) <- paste(geneCNetHop3$source, geneCNetHop3$target, sep = "__")
   geneDNetHop3 <- geneANetHop1[1,]
-  rownames(geneDNetHop3) <- paste(geneDNetHop3$gene, geneDNetHop3$gene, sep = "__")
+  rownames(geneDNetHop3) <- paste(geneDNetHop3$source, geneDNetHop3$target, sep = "__")
   subnetworksFull <- list(geneA = list(geneANetHop1, geneANetHop2, geneANetHop3),
                       geneB = list(geneBNetHop1, geneBNetHop2, geneBNetHop3),
                       geneC = list(geneCNetHop1, geneCNetHop2, geneCNetHop3),
                       geneD = list(geneDNetHop1, geneDNetHop2, geneDNetHop3))
   
-  # Test method with all genes as input.
+    # Test method with all genes as input.
   sigEdges <- FindSignificantEdgesforhopU(geneSet = c("geneA", "geneB", "geneC", "geneD"),
                                          combinedNetwork = subnetwork,
                                          hopConstraint = 3)
